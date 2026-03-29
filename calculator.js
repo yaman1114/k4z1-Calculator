4c19f9c0cf9a1f08f7bd998aa600997879ee007f
Mandip-xd <np03cs4s250044@heraldcollege.edu.np>
2026-03-29 08:22:57 +0545

    Final Commit

--- calculator.js
+++ calculator.js
@@ -1,10 +1,40 @@
-function divide(a, b) {
-    if (b === 0){
-        console.log("Invalid input : Number cant be divided by zero ");
-        return null;
-    }
-    return a / b;
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+let operatorSelected = false;
+
+function multiply(a, b) {
+    return a * b;
 }
 
-//test 
-console.log(divide(10,2));
+function appendNumber(num) {
+    let display = document.getElementById('display');
+    if (operatorSelected) {
+        display.value = '';
+        operatorSelected = false;
+    }
+    display.value += num;
+}
+
+function setOperator(op) {
+    let display = document.getElementById('display');
+    firstNumber = display.value;
+    operator = op;
+    operatorSelected = true;
+}
+
+function calculate() {
+    let display = document.getElementById('display');
+    secondNumber = display.value;
+
+    let num1 = parseFloat(firstNumber);
+    let num2 = parseFloat(secondNumber);
+
+    if (operator === '*') {
+        display.value = multiply(num1, num2);
+    }
+
+    firstNumber = display.value;
+    operator = '';
+    operatorSelected = true;
+}
