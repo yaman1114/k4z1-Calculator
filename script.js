a5d30c9037c8f509e68c36e8579d01a2d886076d
Mandip-xd <np03cs4s250044@heraldcollege.edu.np>
2026-03-29 08:35:12 +0545

    Divison functionality

--- script.js
+++ script.js
@@ -3,3 +3,42 @@ let currentInput = '';
 let previousInput = '';
 let operator = null;
 let shouldResetDisplay = false;
+
+function divide(a, b) {
+    if (b === 0) {
+        return 'Error: Divide by 0';
+    }
+    return a / b;
+}
+
+function appendNumber(num) {
+    let display = document.getElementById('display');
+    if (operatorSelected) {
+        display.value = '';
+        operatorSelected = false;
+    }
+    display.value += num;
+}
+
+function setOperator(op) {
+    let display = document.getElementById('display');
+    firstNumber = display.value;
+    operator = op;
+    operatorSelected = true;
+}
+
+function calculate() {
+    let display = document.getElementById('display');
+    secondNumber = display.value;
+
+    let num1 = parseFloat(firstNumber);
+    let num2 = parseFloat(secondNumber);
+
+    if (operator === '/') {
+        display.value = divide(num1, num2);
+    }
+
+    firstNumber = display.value;
+    operator = '';
+    operatorSelected = true;
+}
