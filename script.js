941941c6f4ef11411c1483b90985af7310c45788
Ayush <np03cs4s250056@heraldcollege.edu.np>
2026-03-29 08:53:56 +0545

    subtract in script

---
+++ script.js
@@ -0,0 +1,40 @@
+let firstNumber = '';
+let secondNumber = '';
+let operator = '';
+let operatorSelected = false;
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function appendNumber(num) {
+    let display = document.getElementById('display');
+    if (operatorSelected) {
+        display.value = '';
+        operatorSelected = false;
+    }
+    display.value += num;
+}
+
+function setOperator(op) {
+    let display = document.getElementById('display');
+    firstNumber = display.value;
+    operator = op;
+    operatorSelected = true;
+}
+
+function calculate() {
+    let display = document.getElementById('display');
+    secondNumber = display.value;
+
+    let num1 = parseFloat(firstNumber);
+    let num2 = parseFloat(secondNumber);
+
+    if (operator === '-') {
+        display.value = subtract(num1, num2);
+    }
+
+    firstNumber = display.value;
+    operator = '';
+    operatorSelected = true;
+}
